c120797a4fe2b6f4e9975af503deed4fb988be92
yassin <me@yassinsabek.dev>
2026-08-27 22:41:20 +0300

    Add initial project structure and configuration files for Online Bookstore Internship Project

---
+++ backend/src/test/java/com/pulsebysolutions/onlinebookstoreinternshipproject/OnlineBookstoreInternshipProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulsebysolutions.onlinebookstoreinternshipproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookstoreInternshipProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
